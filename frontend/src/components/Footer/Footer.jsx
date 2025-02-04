bece89dd383c65f63848048bd3f530a97ce42566
kirin <kirin@safesteps.tech>
2025-02-04 16:33:43 +0000

    add footer and navbar

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Footer.module.scss";
+
+const Footer = () => {
+  return (
+    <>
+    <footer>
+            <p>Footer works</p>
+    </footer>
+
+    </>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./Navbar.module.scss";
+import logo from "./logo.png";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <div className={styles.navbar}>
+         <img  alt="logo" />
+          <ul>
+            <li>Dashboard </li>
+            <li>Tax-Report </li>
+
+            <li>Charity-Search</li>
+            <li className={styles.iconText}>
+            <FontAwesomeIcon icon={faUser} style={{ fontSize: 20 }} />
+            </li>
+
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+export default Navbar;
