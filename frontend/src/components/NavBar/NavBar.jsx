bfd02ba1e95d6fc2c464ce172a6f4c1fa25180d6
kirin <kirin@safesteps.tech>
2025-02-10 17:09:58 +0000

    add jest packages ( for unit testing)

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "./Navbar.module.scss";
-import logo from "./logo.png";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
