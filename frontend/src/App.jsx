c1341e535a6b52f80099dd2ffa99ca5551c59f09
kirin <kirin@safesteps.tech>
2025-04-07 14:09:02 +0100

    add homepage to package.json

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,34 @@
 import React, { useState, useEffect } from "react";
 import CharitySearch from "./containers/CharitySearch/CharitySearch";
 import Dashboard from "./containers/Dashboard/Dashboard";
-import Login from "./containers/Login/Login";
+import Login from "./containers/Login/Login"; 
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 import TaxReporting from "./containers/TaxReporting/TaxReporting";
 import Account from "./containers/Account/Account";
 import LearnMore from "./containers/LearnMore/LearnMore";
+import ForgotPassword from "./containers/ForgotPassword/ForgotPassword";
+import ResetPassword from "./containers/ResetPassword/ResetPassword";
+import PrivacyPolicy from "./containers/PrivacyPolicy/PrivacyPolicy";
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path ="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword/>} />
         <Route path="/LearnMore" element={<LearnMore />} />
+        <Route path="/register" element={<Account />} />     
+           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
 
-        <Route path="/register" element={<Account />} />
-        <Route
-          path="/dashboard"
-          element={<PrivateRoute element={<Dashboard />} />}
-        />
-        <Route
-          path="/CharitySearch"
-          element={<PrivateRoute element={<CharitySearch />} />}
-        />
-        <Route
-          path="/tax-reporting"
-          element={<PrivateRoute element={<TaxReporting />} />}
-        />
+        {/* Updated private route handling */}
+        <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />
+        <Route path="/CharitySearch" element={<PrivateRoute element={<CharitySearch />} />} />
+        <Route path="/tax-reporting" element={<PrivateRoute element={<TaxReporting />} />} />
       </Routes>
     </Router>
   );
 };
+
 export default App;
