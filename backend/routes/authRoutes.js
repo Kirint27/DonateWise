76efed921aac249095de0552a20190d980f922a8
kirin <kirin@safesteps.tech>
2025-04-10 11:10:41 +0100

    test update same site to lax

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -141,7 +141,7 @@ router.post("/login", (req, res) => {
       res.cookie("authToken", token, {
         httpOnly: true, // Now, the token can't be accessed via JavaScript
         secure: process.env.NODE_ENV === "production", // Ensures the cookie is sent over HTTPS in production
-        sameSite: "Strict", // Prevent CSRF
+        sameSite: "Lax",  // Change from "Strict" to "Lax"
         maxAge: 3600000, // 1 hour expiry
       });
 
