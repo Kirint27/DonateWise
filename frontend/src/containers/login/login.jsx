d2e9387d14f0f1d799659581c727f395be64cc94
kirin <kirin@safesteps.tech>
2025-04-24 15:25:48 +0100

    improve  styling of login page

--- frontend/src/containers/login/login.jsx
+++ frontend/src/containers/login/login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styles from "./login.module.scss";
+import styles from "./Login.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import Footer from "../../components/Footer/Footer";
@@ -59,76 +59,82 @@ const Login = ({ onLogin }) => {
         setError(" Wrong email or password"); // Set error message for display
       });
   };
-  return (
-    <div className={styles.pageContainer}>
-      <h1 className="title">DonateWise</h1>
-      <div className={styles.loginContainer}>
-        <div className={styles.loginBox}>
-          <div className={styles.loginLogo}>
-          <img className={styles.logo} src={logo} alt="Logo" />
-          <br />
-            <p className={styles.slogan}>
-              "Track Your Giving, Amplify Your Impact".
-            </p>
-          </div>
-
-          <div className={styles.loginForm}>
-            <h2>Login</h2>
-            <form onSubmit={handleLogin}>
-              <div className={styles.formGroup}>
-                <label>Email</label>
-                <input
-                  type="email"
-                  placeholder="Email"
-                  value={email}
-                  onChange={handleEmailChange}
-                  required
-                />{" "}
+    return (
+      <div className="mainWrapper">
+        <div className={styles.pageContainer}>
+          <div className={styles.loginContainer}>
+            <div className={styles.loginBox}>
+              <div className={styles.loginLogo}>
+                <h1 className="title">DonateWise</h1>
+                <img className={styles.logo} src={logo} alt="Logo" />
+                <br />
+                <p className={styles.slogan}>
+                  "Track Your Giving, Amplify Your Impact".
+                </p>
               </div>
-              <div className={styles.formGroup}>
-                <label>Password</label>
-                <div style={{ position: "relative" }}>
-                  <input
-                    type={isPasswordVisible ? "text" : "password"}
-                    placeholder="Enter password"
-                    value={password}
-                    onChange={handlePasswordChange}
-                    required
-                  />
-                  <span
-                    onClick={togglePasswordVisibility}
-                    className={styles.passwordVisibilityIcon}
-                  >
-                    <FontAwesomeIcon
-                      icon={isPasswordVisible ? faEye : faEyeSlash}
+    
+              <div className={styles.loginForm}>
+                <h2>Login</h2>
+                <form onSubmit={handleLogin}>
+                  <div className={styles.formGroup}>
+                    <label>Email</label>
+                    <input
+                      type="email"
+                      placeholder="Email"
+                      value={email}
+                      onChange={handleEmailChange}
+                      required
                     />
-                  </span>
-                </div>
-                <a className={styles.forgotten}><Link to="/forgot-password">Forgot Password</Link></a>
+                  </div>
+                  <div className={styles.formGroup}>
+                    <label>Password</label>
+                    <div style={{ position: "relative" }}>
+                      <input
+                        type={isPasswordVisible ? "text" : "password"}
+                        placeholder="Enter password"
+                        value={password}
+                        onChange={handlePasswordChange}
+                        required
+                      />
+                      <span
+                        onClick={togglePasswordVisibility}
+                        className={styles.passwordVisibilityIcon}
+                      >
+                        <FontAwesomeIcon
+                          icon={isPasswordVisible ? faEye : faEyeSlash}
+                        />
+                      </span>
+                    </div>
+                    <a className={styles.forgotten}>
+                      <Link to="/forgot-password">Forgot Password</Link>
+                    </a>
+                  </div>
+                  <button className={styles.primaryButton} type="submit">
+                    Login
+                  </button>
+                  {error && (
+                    <p style={{ color: "red", marginTop: "10px" }}>{error}</p>
+                  )}
+                </form>
+                <button
+                  onClick={() => navigate("/register")}
+                  className="secondaryButton"
+                >
+                  Create Account
+                </button>
               </div>
-              <button className={styles.primaryButton} type="submit">
-                Login
-              </button>
-              {error && (
-                <p style={{ color: "red", marginTop: "10px" }}>{error}</p>
-              )}
-            </form>
-            <button
-              onClick={() => navigate("/register")}
-              className="secondaryButton"
-            >
-              Create Account
-            </button>
+            </div>
           </div>
+          <p className={styles.learn}>
+            New to GivingTracker: <Link to="/LearnMore">Learn more</Link>
+          </p>
         </div>
+        <Footer />
       </div>
-      <p className={styles.learn}>
-        New to GivingTracker: <Link to="/LearnMore">Learn more</Link>
-      </p>
-    <Footer />
-    </div>
+    );
+    
 
-  );
+  
 };
 
 export default Login;
