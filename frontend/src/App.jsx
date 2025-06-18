cd69efabe163d5489349204016e5ac6dc76777df
kirin <kirin@safesteps.tech>
2025-06-18 16:23:54 +0100

    add hotjar to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import CharitySearch from "./containers/CharitySearch/CharitySearch";
 import Dashboard from "./containers/Dashboard/Dashboard";
-import Login from "./containers/login/login"; 
+import Login from "./containers/login/login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 import TaxReporting from "./containers/TaxReporting/TaxReporting";
@@ -10,23 +10,41 @@ import LearnMore from "./containers/LearnMore/LearnMore";
 import ForgotPassword from "./containers/ForgotPassword/ForgotPassword";
 import ResetPassword from "./containers/ResetPassword/ResetPassword";
 import PrivacyPolicy from "./containers/PrivacyPolicy/PrivacyPolicy";
-import  UpdateAccount from "./containers/UpdateAccount/UpdateAccount";
+import UpdateAccount from "./containers/UpdateAccount/UpdateAccount";
+import Hotjar from '@hotjar/browser';
+
+const siteId = 6438849;
+const hotjarVersion = 6;
 const App = () => {
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      Hotjar.init(siteId, hotjarVersion);
+    }
+  }, []);
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/update-account" element={<UpdateAccount />} />
-        <Route path ="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password" element={<ResetPassword/>} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/LearnMore" element={<LearnMore />} />
-        <Route path="/register" element={<Account />} />     
-           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/register" element={<Account />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
 
         {/* Updated private route handling */}
-        <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />
-        <Route path="/CharitySearch" element={<PrivateRoute element={<CharitySearch />} />} />
-        <Route path="/tax-reporting" element={<PrivateRoute element={<TaxReporting />} />} />
+        <Route
+          path="/dashboard"
+          element={<PrivateRoute element={<Dashboard />} />}
+        />
+        <Route
+          path="/CharitySearch"
+          element={<PrivateRoute element={<CharitySearch />} />}
+        />
+        <Route
+          path="/tax-reporting"
+          element={<PrivateRoute element={<TaxReporting />} />}
+        />
       </Routes>
     </Router>
   );
