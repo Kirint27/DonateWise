e63f254a9844a7fa4403d6053b2dc4935615d2ad
kirin <kirin@safesteps.tech>
2025-06-19 15:49:48 +0100

    fix issue with hotjar running

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import CharitySearch from "./containers/CharitySearch/CharitySearch";
 import Dashboard from "./containers/Dashboard/Dashboard";
 import Login from "./containers/login/login";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 import TaxReporting from "./containers/TaxReporting/TaxReporting";
 import Account from "./containers/Account/Account";
@@ -15,14 +15,29 @@ import Hotjar from '@hotjar/browser';
 
 const siteId = 6438849;
 const hotjarVersion = 6;
+
+function HotjarRouteTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (window.hj) {
+      window.hj('stateChange', location.pathname);
+    }
+  }, [location]);
+
+  return null;
+}
+
 const App = () => {
   useEffect(() => {
     if (process.env.NODE_ENV === 'production') {
       Hotjar.init(siteId, hotjarVersion);
     }
   }, []);
+
   return (
     <Router>
+      <HotjarRouteTracker /> 
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/update-account" element={<UpdateAccount />} />
@@ -32,19 +47,9 @@ const App = () => {
         <Route path="/register" element={<Account />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
 
-        {/* Updated private route handling */}
-        <Route
-          path="/dashboard"
-          element={<PrivateRoute element={<Dashboard />} />}
-        />
-        <Route
-          path="/CharitySearch"
-          element={<PrivateRoute element={<CharitySearch />} />}
-        />
-        <Route
-          path="/tax-reporting"
-          element={<PrivateRoute element={<TaxReporting />} />}
-        />
+        <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />
+        <Route path="/CharitySearch" element={<PrivateRoute element={<CharitySearch />} />} />
+        <Route path="/tax-reporting" element={<PrivateRoute element={<TaxReporting />} />} />
       </Routes>
     </Router>
   );
