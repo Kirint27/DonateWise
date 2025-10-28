368b9bbacb21b330acfbfdb29625ae37808485d8
kirin <kirin@safesteps.tech>
2025-10-28 11:56:37 +0000

    fix styling of login page

--- frontend/src/containers/login/login.jsx
+++ frontend/src/containers/login/login.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import styles from "./login.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useAuth } from "../Context/authContext";
+
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import Footer from "../../components/Footer/Footer";
 import { useNavigate, Link } from "react-router-dom";
 const Login = ({ onLogin }) => {
+  const { login } = useAuth()
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
@@ -25,63 +28,64 @@ const Login = ({ onLogin }) => {
   const togglePasswordVisibility = () => {
     setIsPasswordVisible(!isPasswordVisible);
   };
-
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
     setError("");
     setLoading(true);
-    console.log("Sending login request with:");
-    console.log(email);
-    console.log(password);
-    const url = `https://charitytrackr.onrender.com/api/login`; // Adjust based on your backend
-
-    fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-      credentials: "include", // Include cookies
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.error) {
-          throw new Error(data.error);
-        }
-        console.log("Navigation triggered");
-
-        setLoading(false);
-        navigate("/dashboard", { replace: true }); // Add { replace: true } to navigate correctly
-      })
-      .catch((err) => {
-        setLoading(false);
-        if (err.message.includes("Account locked")) {
-          setError(err.message);
-        } else {
-          setError("Wrong email or password");
-        }
+  
+    const url = "http://localhost:3001/api/login";
+  
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+        credentials: "include",
       });
+  
+      const data = await response.json();
+  
+      if (data.error) {
+        throw new Error(data.error);
+      }
+  
+      login(data.user);
+      navigate("/dashboard", { replace: true });
+    } catch (err) {
+      if (err.message.includes("Account locked")) {
+        setError(err.message);
+      } else {
+        setError("Wrong email or password");
+      }
+    } finally {
+      setLoading(false);
+    }
   };
+  
+  
   return (
     <div className="mainWrapper">
       <div className={styles.pageContainer}>
         <div className={styles.loginContainer}>
           <div className={`${styles.loginBox} ${styles.mobile}`}>
             <div className={styles.loginLogo}>
-              <h1 className="title">DonateWise</h1>
-              <br />
-              <p className={styles.slogan}>
+              <h1 className="title">DonateWise <br /> <p className={styles.slogan}>
                 "Track Your Giving, Amplify Your Impact".
-              </p>
+              </p></h1>
+  
+              <div className={styles.appDescription}>
+                With DonateWise you can view your donation dashboard with progress and insights, review your full history by tax year, and search for charities by name, location, or cause.
+              </div>
             </div>
-
+  
             <div className={styles.loginForm}>
-              <p className={styles.formTitle}>Login</p>
+              <p className={styles.formTitle}>Login To DonateWise</p>
               <form onSubmit={handleLogin}>
                 <div className={styles.formGroup}>
-                  <label>Email</label>
+                  <label htmlFor="email">Email</label>
                   <input
                     type="email"
+                    id="email"
                     placeholder="Email"
                     value={email}
                     onChange={handleEmailChange}
@@ -89,23 +93,24 @@ const Login = ({ onLogin }) => {
                   />
                 </div>
                 <div className={styles.formGroup}>
-                  <label>Password</label>
+                  <label htmlFor="password">Password</label>
                   <div style={{ position: "relative" }}>
                     <input
                       type={isPasswordVisible ? "text" : "password"}
+                      id="password"
                       placeholder="Enter password"
                       value={password}
                       onChange={handlePasswordChange}
                       required
                     />
                     {/* <span
-                        onClick={togglePasswordVisibility}
-                        className={styles.passwordVisibilityIcon}
-                      >
-                        <FontAwesomeIcon
-                          icon={isPasswordVisible ? faEye : faEyeSlash}
-                        />
-                      </span> */}
+                          onClick={togglePasswordVisibility}
+                          className={styles.passwordVisibilityIcon}
+                        >
+                          <FontAwesomeIcon
+                            icon={isPasswordVisible ? faEye : faEyeSlash}
+                          />
+                        </span> */}
                   </div>
                   <a className={styles.forgotten}>
                     <Link to="/forgot-password">Forgot Password</Link>
@@ -120,7 +125,7 @@ const Login = ({ onLogin }) => {
               </form>
               <button
                 onClick={() => navigate("/register")}
-                className="secondaryButton"
+                className={`secondaryButton ${styles.createAccountButton}`}
               >
                 Create Account
               </button>
